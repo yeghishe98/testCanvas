7aa5f992ef66ae71f5ab9d9567f5c81352b3e00d
yeghisheavagyan <yeghishe.avagyan@picsart.com>
2021-07-09 13:04:25 +0400

    Added load and download buttons

--- src/canvas/test.js
+++ src/canvas/test.js
@@ -1,38 +1,117 @@
 import './test.css';
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-const handleJpg = (canvasRef) => {
-    let ctx = canvasRef.current.getContext('2d');
-    let img = new Image();
-    img.onload = function() {
-        ctx.drawImage(img, 20, 30, 120, 90);
-    }
-    img.src = "https://cdn.statically.io/img/i.pinimg.com/originals/7d/9f/8b/7d9f8b929315d50ddae5c2879bb17a16.jpg";
-}
-
-const handlePng = (canvasRef) => {
-    let ctx = canvasRef.current.getContext('2d');
-    let img = new Image();
-    img.onload = function() {
-        ctx.drawImage(img, 160, 30, 120, 90);
-    }
-    img.src = "https://www.freepnglogos.com/uploads/tiger-png/tiger-png-2.png";
+// const handleJpg = (canvasRef) => {
+//     let ctx = canvasRef.current.getContext('2d');
+//     let img = new Image();
+//     img.onload = function() {
+//         ctx.drawImage(img, 20, 30, 120, 90);
+//     }
+//     img.src = "https://cdn.statically.io/img/i.pinimg.com/originals/7d/9f/8b/7d9f8b929315d50ddae5c2879bb17a16.jpg";
+// }
+//
+// const handlePng = (canvasRef) => {
+//     let ctx = canvasRef.current.getContext('2d');
+//     let img = new Image();
+//     img.onload = function() {
+//         ctx.drawImage(img, 160, 30, 120, 90);
+//     }
+//     img.src = "https://www.freepnglogos.com/uploads/tiger-png/tiger-png-2.png";
+// }
+//
+const obj_format = {
+  jpg: "image/jpeg",
+  jpeg: "image/jpeg",
+  png: "image/png"
 }
 
 function Foo() {
 
-  let canvasRef = useRef(null);
+  const [format, setFormat] = useState('jpg');
 
-    return(
+  const [choose, setChoose] = useState(false);
+
+  const [coaliti, setCoaliti] = useState(1);
+
+  const inputRef = useRef(null);
+
+  const canvasRef = useRef(null);
+
+  const handleChangeChoose = () => {
+    const file = inputRef.current.files[0];
+
+    const reader = new FileReader();
+    const ctx = canvasRef.current.getContext('2d');
+
+    reader.addEventListener("load", function () {
+      const image = new Image();
+      image.src = reader.result;
+      image.onload = function() {
+           ctx.drawImage(image, 10, 10, 320, 200);
+         }
+    }, false);
+
+    reader.readAsDataURL(file);
+
+  }
+
+
+
+   const downloadImage = () => {
+     let x = obj_format[format];
+     console.log(x);
+     const canvas = canvasRef.current;
+     const image = canvas.toDataURL(x, coaliti);
+     const a = document.createElement('a');
+     a.href = image;
+     a.download = 'filename';
+     document.body.appendChild(a);
+     a.click();
+  }
+
+  const handleChangeFormat = (e) => {
+    setFormat(e.target.value);
+  }
+
+  const handleCoaliti = (e) => {
+    setCoaliti(Number(e.target.value));
+  }
+
+  const handleChooseChange = () => {
+    setChoose(true);
+  }
+
+  return (
+    <div>
+      <canvas ref={canvasRef} width={1000} height={500} />
+
       <div>
-          <canvas ref={canvasRef} className="canvas" />
-          <div className="buttons">
-              <button className="but_jpg" onClick={() => handleJpg(canvasRef)}>JPG</button>
-              <button className="but_png" onClick={() => handlePng(canvasRef)}>PNG</button>
-          </div>
-      </div>
+        <input type="file" ref={inputRef} onChange={handleChooseChange} />
 
-    )
+        {choose ?
+          (
+            <>
+              <button onClick={handleChangeChoose}>Load Image</button>
+
+              <select onChange={handleChangeFormat}>
+                <option value="jpg">JPG</option>
+                <option value="png">PNG</option>
+              </select>
+
+              {format === 'jpg' ?
+                <select onChange={handleCoaliti}>
+                  <option value={1}>1</option>
+                  <option value={0.5}>0.5</option>
+                  <option value={0}>0</option>
+                </select> : null
+              }
+
+              <button onClick={downloadImage}>Download</button>
+            </>
+          ) : null}
+      </div>
+    </div>
+  )
 }
 export default Foo
 
